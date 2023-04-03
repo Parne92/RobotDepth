efec6e6b2bdb4606cfc4c9dcf6f008bfce6d5627
Nathan Parnell <52173148+Parne92@users.noreply.github.com>
2023-04-03 09:12:45 -0600

    Update project3.py

--- Project3/project3.py
+++ Project3/project3.py
@@ -119,14 +119,12 @@ try:
         cv2.circle(blank_image2, (640,240), (5), (0, 0, 255), 2, 1)
         images = np.vstack((images,blank_image2))
 
-
-        print (depth_frame.get_distance( math.floor((bbox[0]+bbox[2])/2), math.floor((bbox[1]+bbox[3])/2) )*100)
-
         xCoord = (bbox[0] + bbox[2]/2)
         xCoord = math.floor(xCoord)
         yCoord = (bbox[1]+ bbox[3]/2)
         yCoord = math.floor(yCoord)
         distance = depth_frame.get_distance(xCoord,yCoord)
+        print(distance)
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
@@ -137,11 +135,13 @@ try:
             if(motors >7900):
                    motors = 7900
             tango.setTarget(MOTORS,motors)
+            print("Moving forwards")
         elif(distance > 1):
             motors -= 200
             if(motors < 1510):
                 motors = 1510
             tango.setTarget(MOTORS,motors)
+            print("Moving Backwards")
 
             
 
