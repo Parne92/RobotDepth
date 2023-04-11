198f5f925e7e850c1850f8ca73c9f54f1d21f39d
Parne92 <nathan_parnell3@hotmail.com>
2023-04-11 17:23:23 -0600

    still working

--- Project4/project4.py
+++ Project4/project4.py
@@ -87,7 +87,7 @@ try:
 
         gray = cv2.cvtColor(normalize,cv2.COLOR_BGR2GRAY)
 
-        edge = cv2.Canny(gray,750,1000)
+        edge = cv2.Canny(gray,350,400)
 
 
         #--Threshold Image--
