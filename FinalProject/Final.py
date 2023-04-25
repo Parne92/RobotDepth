c34aad73756d86c0d8f0de593e86972212bf880d
Parne92 <nathan_parnell3@hotmail.com>
2023-04-24 21:02:36 -0600

    New Color Detection

--- FinalProject/Final.py
+++ FinalProject/Final.py
@@ -57,100 +57,39 @@ frames = pipeline.wait_for_frames()
 aligned_frames = align.process(frames)
 color_frame = frames.get_color_frame()
 
+
 # Convert images to numpy arrays
 color_image = np.asanyarray(color_frame.get_data())
 
+try:
+    while True:
 
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+
+        # Convert images to numpy arrays
+        color_image = np.asanyarray(color_frame.get_data())
+
+        color_colormap_dim = color_image.shape
+
+        # Convert Image to Image HSV
+        hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
+
+        # Defining lower and upper bound HSV values
+        lower = np.array([50, 100, 100])
+        upper = np.array([70, 255, 255])
   
-  
-# Start a while loop
-while(1):
-  
-    # Convert the imageFrame in 
-    # BGR(RGB color space) to 
-    # HSV(hue-saturation-value)
-    # color space
-    hsvFrame = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
-  
-    # Set range for red color and 
-    # define mask
-    red_lower = np.array([136, 87, 111], np.uint8)
-    red_upper = np.array([180, 255, 255], np.uint8)
-    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
-  
-    # Set range for green color and 
-    # define mask
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
-  
-    # Set range for blue color and
-    # define mask
-    blue_lower = np.array([94, 80, 2], np.uint8)
-    blue_upper = np.array([120, 255, 255], np.uint8)
-    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
-      
-    # Morphological Transform, Dilation
-    # for each color and bitwise_and operator
-    # between imageFrame and mask determines
-    # to detect only that particular color
-    kernel = np.ones((5, 5), "uint8")
-      
-    # For red color
-    red_mask = cv2.dilate(red_mask, kernel)
-    res_red = cv2.bitwise_and(color_image, color_image, 
-                              mask = red_mask)
-      
-    # For green color
-    green_mask = cv2.dilate(green_mask, kernel)
-    res_green = cv2.bitwise_and(color_image, color_image,
-                                mask = green_mask)
-      
-    # For blue color
-    blue_mask = cv2.dilate(blue_mask, kernel)
-    res_blue = cv2.bitwise_and(color_image, color_image,
-                               mask = blue_mask)
-   
-    # Creating contour to track red color
-    contours, hierarchy = cv2.findContours(red_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-      
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            color_image = cv2.rectangle(color_image, (x, y), 
-                                       (x + w, y + h), 
-                                       (0, 0, 255), 2) 
-  
-    # Creating contour to track green color
-    contours, hierarchy = cv2.findContours(green_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-      
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            color_image = cv2.rectangle(color_image, (x, y), 
-                                       (x + w, y + h),
-                                       (0, 255, 0), 2)
-  
-    # Creating contour to track blue color
-    contours, hierarchy = cv2.findContours(blue_mask,
-                                           cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        if(area > 300):
-            x, y, w, h = cv2.boundingRect(contour)
-            color_image = cv2.rectangle(color_image, (x, y),
-                                       (x + w, y + h),
-                                       (255, 0, 0), 2)
-              
-    # Program Termination
-    cv2.imshow("Multiple Color Detection in Real-TIme", color_image)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        # Defining mask for detecting color
+        mask = cv2.inRange(hsv, lower, upper)
+
+        # Display Image and Mask
+        cv2.imshow("Image", color_image)
+        cv2.imshow("Mask", mask)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+finally:
+
+    # Stop streaming
+    pipeline.stop()
