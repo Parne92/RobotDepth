b464d2ca6791850af98e750c784982dea13891ad
Parne92 <nathan_parnell3@hotmail.com>
2023-04-24 21:20:09 -0600

    Rework Color

--- FinalProject/Final.py
+++ FinalProject/Final.py
@@ -69,23 +69,94 @@ try:
         color_frame = frames.get_color_frame()
 
         # Convert images to numpy arrays
-        color_image = np.asanyarray(color_frame.get_data())
+        _,color_image = np.asanyarray(color_frame.get_data())
 
         color_colormap_dim = color_image.shape
 
         # Convert Image to Image HSV
         hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
-        # Defining lower and upper bound HSV values
-        lower = np.array([50, 100, 100])
-        upper = np.array([70, 255, 255])
+        # Set range for red color and 
+        # define mask
+        red_lower = np.array([136, 87, 111], np.uint8)
+        red_upper = np.array([180, 255, 255], np.uint8)
+        red_mask = cv2.inRange(hsv, red_lower, red_upper)
   
-        # Defining mask for detecting color
-        mask = cv2.inRange(hsv, lower, upper)
+        # Set range for green color and 
+        # define mask
+        green_lower = np.array([25, 52, 72], np.uint8)
+        green_upper = np.array([102, 255, 255], np.uint8)
+        green_mask = cv2.inRange(hsv, green_lower, green_upper)
+  
+        # Set range for blue color and
+        # define mask
+        blue_lower = np.array([94, 80, 2], np.uint8)
+        blue_upper = np.array([120, 255, 255], np.uint8)
+        blue_mask = cv2.inRange(hsv, blue_lower, blue_upper)
+
+        # Morphological Transform, Dilation
+        # for each color and bitwise_and operator
+        # between imageFrame and mask determines
+        # to detect only that particular color
+        kernel = np.ones((5, 5), "uint8")
+      
+        # For red color
+        red_mask = cv2.dilate(red_mask, kernel)
+        res_red = cv2.bitwise_and(color_image, color_image, 
+                              mask = red_mask)
+      
+        # For green color
+        green_mask = cv2.dilate(green_mask, kernel)
+        res_green = cv2.bitwise_and(color_image, color_image,
+                                mask = green_mask)
+      
+        # For blue color
+        blue_mask = cv2.dilate(blue_mask, kernel)
+        res_blue = cv2.bitwise_and(color_image, color_image,
+                               mask = blue_mask)
+   
+        # Creating contour to track red color
+        contours, hierarchy = cv2.findContours(red_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+        
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > 300):
+                x, y, w, h = cv2.boundingRect(contour)
+                color_image = cv2.rectangle(color_image, (x, y), 
+                                       (x + w, y + h), 
+                                       (0, 0, 255), 2)
+                
+
+        # Creating contour to track green color
+        contours, hierarchy = cv2.findContours(green_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+      
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > 300):
+                x, y, w, h = cv2.boundingRect(contour)
+                color_image = cv2.rectangle(color_image, (x, y), 
+                                       (x + w, y + h),
+                                       (0, 255, 0), 2)
+                
+
+        contours, hierarchy = cv2.findContours(blue_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+        for pic, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if(area > 300):
+                x, y, w, h = cv2.boundingRect(contour)
+                color_image = cv2.rectangle(color_image, (x, y),
+                                       (x + w, y + h),
+                                       (255, 0, 0), 2)
+              
 
         # Display Image and Mask
         cv2.imshow("Image", color_image)
-        cv2.imshow("Mask", mask)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
