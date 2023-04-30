ef92877aec6a474de7cd14929a7b2cfa0bdce6d8
Parne92 <nathan_parnell3@hotmail.com>
2023-04-30 15:46:08 -0600

    undo hardcode

--- FinalProject/Final.py
+++ FinalProject/Final.py
@@ -59,7 +59,7 @@ color_image = np.asanyarray(color_frame.get_data())
 
 face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
 
-inMiningArea = True
+inMiningArea = False
 foundFace = False
 savedColor = None
 firstLoop = True
